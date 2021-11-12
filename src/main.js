ac464daf7943972cd17f36b6d90ab6c6682241be
td_code <1098691502@qq.com>
2021-11-13 00:31:59 +0800

    注释

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import './plugins/element.js'
 // 全局的一个引入
 import './assets/css/reset.less'
+// 引入阿里的图标库 css文件
 import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 
