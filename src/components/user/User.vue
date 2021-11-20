cb31ad287ec451a758fcc0cc3cd6f7c72e8619ca
td_code <1098691502@qq.com>
2021-11-20 16:38:41 +0800

    界面展示完成

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -55,6 +55,15 @@
           </el-tooltip>
         </el-table-column>
       </el-table>
+        <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
     </el-card>
   </div>
 </template>
