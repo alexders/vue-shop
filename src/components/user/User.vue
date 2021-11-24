d6bfd3b761dfd074991e3bf8efb764cbc4d91e43
xxx <xxxx@qq.com>
2021-11-24 20:07:37 +0800

    添用户框完成

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -11,22 +11,37 @@
       <div style="margin-top: 15px">
         <el-row :gutter="20">
           <el-col :span="8">
-            <el-input placeholder="请输入用户名" class="input-with-select" v-model="queryInfo.query" clearable  @clear="getUserList" >
+            <el-input
+              placeholder="请输入用户名"
+              class="input-with-select"
+              v-model="queryInfo.query"
+              clearable
+              @clear="getUserList"
+            >
               <el-button
                 slot="append"
                 icon="el-icon-search"
-                @click="getUserList" 
-              ></el-button > </el-input
+                @click="getUserList"
+              ></el-button> </el-input
           ></el-col>
           <el-col :span="4"
-            ><el-button type="primary" class="addUser">
+            ><el-button
+              type="primary"
+              class="addUser"
+              @click="addDialogVisible = true"
+            >
               添加用户</el-button
             ></el-col
           >
         </el-row>
       </div>
       <!-- 添加用户列表 -->
-      <el-table :data="userList" :stripe="true" :border="true" class="userTable">
+      <el-table
+        :data="userList"
+        :stripe="true"
+        :border="true"
+        class="userTable"
+      >
         <el-table-column type="index" label="序号"> </el-table-column>
         <el-table-column prop="username" label="用户名"> </el-table-column>
         <el-table-column prop="mobile" label="手机号"> </el-table-column>
@@ -36,7 +51,11 @@
         <el-table-column label="状态">
           <!-- 自定义模板使用作用域插槽 -->
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state" @change="changeState(scope.row)"> </el-switch>
+            <el-switch
+              v-model="scope.row.mg_state"
+              @change="changeState(scope.row)"
+            >
+            </el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180">
@@ -56,15 +75,53 @@
           </el-tooltip>
         </el-table-column>
       </el-table>
-        <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="queryInfo.pagenum"
-      :page-sizes="[1, 2, 5, 10]"
-      :page-size="queryInfo.pagesize"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
+      <!-- 分页展示 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+      <!-- 添加用户弹窗 -->
+      <el-dialog
+        title="添加用户"
+        :visible.sync="addDialogVisible"
+        width="50%"
+        :before-close="handleClose"
+      >
+        <!-- 填写表单 -->
+        <el-form
+          :model="userForm"
+          :rules="userRules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="userForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="userForm.password" show-password></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="userForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号" prop="mobile">
+            <el-input v-model="userForm.mobile"></el-input>
+          </el-form-item>
+        </el-form>
+        <!-- 底部按钮 -->
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="addDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addDialogVisible = false"
+            >确 定</el-button
+          >
+        </span>
+      </el-dialog>
     </el-card>
   </div>
 </template>
@@ -80,22 +137,68 @@ export default {
       },
       userList: [],
       // 总数
-      total:0,
+      total: 0,
+      // 弹出框是否展示
+      addDialogVisible: false,
+      //添加用户的表单
+      userForm: {},
+      // 表单的校验
+      userRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 10,
+            message: "长度在6 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          {
+            pattern: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+            message: "邮箱格式不正确",
+            trigger: "blur",
+          },
+        ],
+        mobile: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          {
+            pattern:
+              /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+            message: "手机号格式不正确",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
   methods: {
+    // 添加用户方法
+
     //监听用户状态更新
-   async changeState(userinfo){
+    async changeState(userinfo) {
       // 发请求更新用户状态
-     const {data:res}= await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
-     if (res.meta.status!=200) {
+      const { data: res } = await this.$http.put(
+        `users/${userinfo.id}/state/${userinfo.mg_state}`
+      );
+      if (res.meta.status != 200) {
         // 将按钮状态重置回去
-        userinfo.mg_state=!userinfo.mg_state;
-        this.$message.error("获取失败")
-     }else{
-       this.$message.success("用户状态更新成功")
-     }
-    }, 
+        userinfo.mg_state = !userinfo.mg_state;
+        this.$message.error("获取失败");
+      } else {
+        this.$message.success("用户状态更新成功");
+      }
+    },
 
     // 发送请求获取用户信息列表
     async getUserList() {
@@ -107,19 +210,19 @@ export default {
         this.$message.error("获取用户列表数据异常");
       }
       this.userList = res.data.users;
-      this.total=res.data.total;
+      this.total = res.data.total;
       console.log(this.userList);
     },
     // 监听页面大小变化的方法
-    handleSizeChange(pageSize){
-      this.queryInfo.pagesize=pageSize;
+    handleSizeChange(pageSize) {
+      this.queryInfo.pagesize = pageSize;
       this.getUserList();
     },
     // 监听改变当前页的方法
-    handleCurrentChange(currenPage){
-      this.queryInfo.pagenum=currenPage;
+    handleCurrentChange(currenPage) {
+      this.queryInfo.pagenum = currenPage;
       this.getUserList();
-    }
+    },
   },
   created() {
     this.getUserList();
