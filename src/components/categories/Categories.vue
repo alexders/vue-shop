c662f0343f1e790f97cb083def9418ea0d093033
xxx <xxxx@qq.com>
2022-01-23 21:00:28 +0800

    商品分类

--- src/components/categories/Categories.vue
+++ src/components/categories/Categories.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>categories</div>
+  <div class="categoriesContainer">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="categories-card">
+      <el-button type="primary" class="categoriesBtn" @click="addDialogVisible">添加分类</el-button>
+      <!-- 分类列表 -->
+      <!-- 分页展示 -->
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
