1303d8af1b4ad10beee59d0353328209e9fddb98
xxx <xxxx@qq.com>
2022-01-29 16:02:54 +0800

    分类页面

--- src/components/categories/Categories.vue
+++ src/components/categories/Categories.vue
@@ -27,7 +27,15 @@
         </template>
       </tree-table>
       <!-- 添加分类弹出框 -->
-      <el-dialog title="添加分类" :visible.sync="addCatgoriesDailog" close="">
+      <el-dialog title="添加分类" :visible.sync="addCatgoriesDailog" @close="closeCatDialog">
+        <el-form :model="catForm" :rules="catRules" ref="catForm" label-width="100px" class="demo-catForm">
+          <el-form-item label="分类名称" prop="cat_name">
+            <el-input v-model="catForm.cat_name"></el-input>
+          </el-form-item>
+          <el-form-item label="父级分类" prop="cat_pid">
+            <el-input v-model="catForm.cat_pid"></el-input>
+          </el-form-item>
+        </el-form>
         <!-- 底部按钮 -->
         <span slot="footer" class="dialog-footer">
           <el-button @click="addCatgoriesDailog = false">取 消</el-button>
@@ -35,6 +43,16 @@
         </span>
       </el-dialog>
       <!-- 分页展示 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="index.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="index.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -55,9 +73,25 @@ export default {
         pagesize: 5,
       },
       addCatgoriesDailog: false,
+      catForm: {
+        cat_name: "",
+        cat_pid: "",
+      },
+      catRules: {
+        cat_name: [
+          { required: true, message: "请输入分类名称", trigger: "blur" },
+          {
+            min: 1,
+            max: 10,
+            message: "长度在 1 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
   methods: {
+    // 获取商品分类信息
     async getCategories() {
       const { data: res } = await this.$http.get("categories", {
         params: this.index,
@@ -65,12 +99,22 @@ export default {
       if (res.meta.status != 200) {
         return this.$message.error("获取商品信息失败");
       }
-      console.log(res.data);
       this.categoriesList = res.data.result;
+      this.total=this.categoriesList.length;
     },
     showAddDialog() {
       this.addCatgoriesDailog = true;
     },
+    // 关闭对话框要清空弹窗
+    closeCatDialog() {
+      this.$refs.catForm.resetFields();
+      this.addCatgoriesDailog = false;
+    },
+
+    // 分页展示的方法
+    handleSizeChange(){},
+    handleCurrentChange(){},
+
   },
   created() {
     this.getCategories();
